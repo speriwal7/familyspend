b0263e777177c76e5d9d867b479e251df86e8a02
speriwal7 <shubhamperiwal9777@gmail.com>
2026-05-04 14:50:49 +0530

    allow negative amounts

--- sw.js
+++ sw.js
@@ -1,4 +1,4 @@
-const CACHE = 'familyspend-v1';
+const CACHE = 'familyspend-v2';
 const FILES = ['./index.html', './manifest.json', './icon-192.png', './icon-512.png'];
 self.addEventListener('install', e => e.waitUntil(caches.open(CACHE).then(c => c.addAll(FILES).catch(()=>{}))));
 self.addEventListener('fetch', e => e.respondWith(caches.match(e.request).then(r => r || fetch(e.request))));
